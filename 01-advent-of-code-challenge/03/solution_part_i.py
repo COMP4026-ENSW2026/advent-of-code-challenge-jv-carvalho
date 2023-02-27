ec72d2b797b1828b906417f574a446f407a1792b
jv-carvalho <69177483+jv-carvalho@users.noreply.github.com>
2023-02-27 00:40:42 -0300

    Resolução parte 1.

--- 01-advent-of-code-challenge/03/solution_part_i.py
+++ 01-advent-of-code-challenge/03/solution_part_i.py
@@ -1,1 +1,19 @@
+def priority(c):
+    if c.islower():
+        return ord(c) - ord('a') + 1
+    else:
+        return ord(c) - ord('A') + 27
 
+with open('input.txt', 'r') as f:
+    rucksacks = [line.strip() for line in f]
+
+total_priority = 0
+
+for rucksack in rucksacks:
+    first_compartment = rucksack[:len(rucksack)//2]
+    second_compartment = rucksack[len(rucksack)//2:]
+    common_items = set(first_compartment) & set(second_compartment)
+    for item in common_items:
+        total_priority += priority(item)
+
+print(total_priority)
